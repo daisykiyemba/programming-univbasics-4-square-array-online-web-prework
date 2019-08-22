8951ffc7df05771ea892ac1cdc0e4b315461ee29
daisy Kiyemba <deedaisy29@gmail.com>
2019-08-22 13:35:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,13 @@
 def square_array(array)
   # your code here
-  
-empty_array = []
-
-array.each do |num|
-  empty_array.push(num**2)
-end
-  return empty_array
- array.each{|array| puts "#{array}"} 
+  counter = 0
+ new_array = []
  
+ while counter < array.length do
+   new_array << array[counter]**2 
+   counter +=1
+end
+
+p new_array
+
 end
