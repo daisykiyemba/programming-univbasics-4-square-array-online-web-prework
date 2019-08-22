3a74db7fdfef075f029bb6619b5e22cec03313aa
daisy Kiyemba <deedaisy29@gmail.com>
2019-08-22 13:28:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  
+empty_array = []
+
+array.each do |num|
+  empty_array.push(num**2)
+end
+  return empty_array
+ array.each{|array| puts "#{array}"} 
+ 
 end
